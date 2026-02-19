90aa3b27e5080399c6104b98054586fcdd4cadea
Asjiad Farooq <asjadfarooq22@gmail.com>
2026-02-19 15:44:05 +0300

    Refactor backend structure: move notes controller and routes to src directory, update server configuration

--- Backend/src/Routes/NotesRoutes.js
+++ Backend/src/Routes/NotesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllNotes, createNote, updateNote, deleteNote } from "../Controllers/notesController";
+import { getAllNotes, createNote, updateNote, deleteNote } from "../Controllers/notesController.js";
 
 const router = express.Router();
 
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import notesRoutes from './Routes/NotesRoutes';
+import notesRoutes from './Routes/NotesRoutes.js';
 const app = express();
 
-
-app.use("/", notesRoutes);
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
   console.log('Server is running on port 5001');
