64d655c44bf661d9cde00f07d7bbc8f143833de4
Asjiad Farooq <asjadfarooq22@gmail.com>
2026-02-23 18:57:01 +0300

    Refactor server.js to connect to the database after initializing the app and improve error handling

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,7 +7,6 @@ import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 const app = express();
-connectDB();
 
 app.use(express.json());
 
@@ -15,6 +14,11 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+connectDB().then( () => {
 app.listen(5001, () => {
   console.log('Server is running on port 5001');
 })
+}).catch( (e) => {
+    console.error("Failed to connect to the database", e);
+    process.exit(1);
+});
