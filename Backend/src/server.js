15b8989e554b21c68ea26c969c9b672046a8c08d
Asjiad Farooq <asjadfarooq22@gmail.com>
2026-02-24 21:18:31 +0300

    chore: remove unused .gitignore files from Backend and Frontend; add start script to package.json; update server.js to serve static files in production and conditionally enable CORS; modify axios base URL for environment compatibility

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path'
 
 import notesRoutes from './Routes/NotesRoutes.js';
 import { connectDB } from './Config/db.js';
@@ -9,20 +10,29 @@ import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-}));
+if(process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: 'http://localhost:5173',
+  }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
+}
 
-connectDB().then( () => {
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
-})
-}).catch( (e) => {
-    console.error("Failed to connect to the database", e);
-    process.exit(1);
+connectDB().then(() => {
+  app.listen(5001, () => {
+    console.log('Server is running on port 5001');
+  })
+}).catch((e) => {
+  console.error("Failed to connect to the database", e);
+  process.exit(1);
 });
