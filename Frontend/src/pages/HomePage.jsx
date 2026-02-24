531565ecfd53ab430a005f63725598e2dc9eac82
Asjiad Farooq <asjadfarooq22@gmail.com>
2026-02-24 15:48:10 +0300

    feat: add delete functionality to NoteCard component and handle empty notes state in HomePage

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from "../componenets/RateLimitedUI";
 import toast from "react-hot-toast";
 import NoteCard from "../componenets/NoteCard";
 import api from "../lib/axios";
+import NotesNotFound from "../componenets/NotesNotFound";
 
 
 
@@ -40,10 +41,11 @@ const HomePage = () => {
 
       <div className="max-w-7xl mx-auto p-4  mt-6">
         {loading && <div className="text-center text-primary py-10">Loading Notes...</div>}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map(note => (
-              <NoteCard key={note._id} note={note}/>
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
 
