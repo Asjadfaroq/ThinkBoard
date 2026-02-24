1a08477a3ca5725a3f9d6faa8da6df157975e9bd
Asjiad Farooq <asjadfarooq22@gmail.com>
2026-02-24 14:22:47 +0300

    Populating data with Ui in Main ThinkBoard

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from "../componenets/navbar"
 import RateLimitedUI from "../componenets/RateLimitedUI";
 import axios from "axios";
 import toast from "react-hot-toast";
+import NoteCard from "../componenets/NoteCard";
 
 
 const HomePage = () => {
@@ -35,6 +36,18 @@ const HomePage = () => {
       <Navbar/>
 
       {isRateLimited && <RateLimitedUI/>}
+
+      <div className="max-w-7xl mx-auto p-4  mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading Notes...</div>}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map(note => (
+              <NoteCard key={note._id} note={note}/>
+            ))}
+          </div>
+
+        )}
+      </div>
     </div>
   );
 };
