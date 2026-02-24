fe53c983c6950af0cca09f4328eacef6c494a098
Asjiad Farooq <asjadfarooq22@gmail.com>
2026-02-24 13:40:50 +0300

    Fixed Cors Issues

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 import notesRoutes from './Routes/NotesRoutes.js';
 import { connectDB } from './Config/db.js';
-import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 
 
@@ -9,8 +11,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-
 app.use(rateLimiter);
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 
 app.use("/api/notes", notesRoutes);
 
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "../componenets/navbar"
 import RateLimitedUI from "../componenets/RateLimitedUI";
+import axios from "axios";
 const HomePage = () => {
   const [isRateLimited, setRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes")
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error fetching notes:", error);
+      }
+    }
+    fetchNotes();
+  }, []);
   return (
     <div className="min-h-screen">
       <Navbar/>
